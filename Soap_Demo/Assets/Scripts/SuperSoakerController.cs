37390b6d05378035393e4885eaca4687a048c643
Marthijn <me.vandeelen@student.han.nl>
2020-10-10 14:05:50 +0200

    Supersoaker now fires "bullets"
    Bullet need to be textured and formed

--- Soap_Demo/Assets/Scripts/SuperSoakerController.cs
+++ Soap_Demo/Assets/Scripts/SuperSoakerController.cs
@@ -1,18 +1,37 @@
-﻿using System.Collections;
+﻿using JetBrains.Annotations;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SuperSoakerController : MonoBehaviour
 {
+
+    public GameObject bullet;
+    private GameObject instantiadBullet;
+    public float bulletVelocity;
+    public float firerate;
+    private bool firing = false;
+    private float time;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("REEE");
+        time = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       if(Input.GetMouseButton(0) && Time.time - time > 60/firerate)
+        {
+            
+            Vector3 position = transform.position;
+            Quaternion rotation = transform.rotation;
+            instantiadBullet = Instantiate<GameObject>(bullet, position, rotation);
+            Vector3 bulletTrajectory = transform.forward * bulletVelocity;
+            instantiadBullet.GetComponent<Rigidbody>().AddForce(bulletTrajectory, ForceMode.Impulse);
+            Destroy(instantiadBullet, 3);
+            time = Time.time;
+        }
     }
 }
