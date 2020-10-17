d16bef70f66f9189e440bdfeb72299f1677be559
Marthijn van Deelen <marthijnvandeelen@gmail.com>
2020-10-17 12:36:28 +0200

    Added particle system to bullet impact

---
+++ Soap_Demo/Assets/Scripts/BulletController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+
+    public GameObject splash;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("Ground"))
+        {
+            Splash();
+            Destroy(gameObject);
+        }    
+    }
+
+    void Splash()
+    {
+        Quaternion quaternion = Quaternion.identity;
+        quaternion = Quaternion.AngleAxis(-90f, Vector3.right);
+        GameObject sploosh = Instantiate(splash, transform.position, quaternion);
+        sploosh.GetComponent<ParticleSystem>().Play();
+        Destroy(sploosh, 2); 
+    }
+}
