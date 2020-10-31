4a6c3d1a5186b03ff06963fe327553f8c91368eb
Marthijn van Deelen <marthijnvandeelen@gmail.com>
2020-10-31 16:24:34 +0100

    Added a object pickup system and crosshair

---
+++ Soap_Demo/Assets/Scripts/GrabSystem.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabSystem : MonoBehaviour
+{
+    [SerializeField]
+    private Transform slot;
+    private PickableObject pickedItem = null;
+
+    public float grabDistance = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            if(pickedItem)
+            {
+                DropItem(pickedItem);
+            }
+            else
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, grabDistance))
+                {
+                    Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                    var pickable = hit.transform.GetComponent<PickableObject>();
+                    if (pickable)
+                    {
+                        Debug.Log("Trying to pick up chemical");
+                        PickItem(pickable);
+
+                    }
+                }
+            }
+        }
+    }
+
+
+    void PickItem(PickableObject item)
+    {
+        pickedItem = item;
+        if(item == null)
+        {
+            Debug.Log("REEEEEEEEEEEEEEE");
+        }
+
+        item.Rb.isKinematic = true;
+        item.Rb.velocity = Vector3.zero;
+        item.Rb.angularVelocity = Vector3.zero;
+        item.Rb.detectCollisions = false;
+
+        item.transform.SetParent(slot);
+
+        item.transform.localPosition = Vector3.zero;
+        item.transform.localEulerAngles = Vector3.zero;
+    }
+
+    void DropItem(PickableObject item)
+    {
+        pickedItem = null;
+        item.transform.SetParent(null);
+        item.Rb.isKinematic = false;
+        item.Rb.detectCollisions = true;
+        item.Rb.AddForce(item.transform.forward * 2, ForceMode.VelocityChange);
+    }
+}
---
+++ Soap_Demo/Assets/Scripts/PickableObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PickableObject : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public Rigidbody Rb => rb;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+}
